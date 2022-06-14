82e704e8f762fcc056ba76c8e9384166bdab28a7
Daniel Zamago <danielzamago@iMac-de-Daniel-2.local>
2022-06-14 10:07:36 -0600

    prueba

---
+++ cursophp/misContactos/php/pruebas_bd_php.php
@@ -0,0 +1,4 @@
+<?php
+    $conexion = mysql_connect("127.0.0.1", "root", "root") or die("Conexión fallida loco :(");
+    echo "Si se hizo la conexión loco :)";
+?>
--- cursophp/misContactos/sql/contactos.sql
+++ cursophp/misContactos/sql/contactos.sql
@@ -1,10 +1,10 @@
-create database contactos;
-use contactos;
+create database contacto;
+use contacto;
 
 create table contactos (email varchar(120) primary key not null, nombre varchar(120) not null, 
 sexo char(1), nacimiento date, telefono varchar(13), pais varchar(50) not null, imagen varchar(50),
-FULLTEXT key buscador (email, nombre, sexo, telefono, pais)) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+FULLTEXT key buscador (email, nombre, sexo, telefono, pais));
 
-create table paises (idPais int auto_increment primary key, pais varchar(50) not null) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+create table paises (idPais int, pais varchar(50) primary key not null)
 
 insert into paises values (0,'México'),(0,'Estados Unidos'),(0,'Francia'),(0,'Cuba'), (0,'Canada');
